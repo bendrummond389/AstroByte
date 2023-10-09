55a004892fe053b48883a7f6524fd36dc4af67bf
bendrummond389 <60265150+bendrummond389@users.noreply.github.com>
2023-10-09 17:48:56 -0600

    made the text div's wider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,12 @@ export default function Home() {
       <div className="flex flex-col md:flex-row items-center text-center max-w-2xl md:max-w-4xl mx-auto">
         <div className="flex-col mr-0 md:mr-12 mb-12 md:mb-0">
           <div className={playfair.className}>
-            <span className="text-4xl md:text-6xl lg:text-7xl font-semibold inline-block w-full md:w-1/2 lg:w-2/3">
+            <span className="text-4xl md:text-6xl lg:text-7xl font-semibold inline-block w-full md:w-2/3 lg:w-4/5">
               Get Ready To Launch With AstroByte
             </span>
           </div>
           <div className={sourceSans.className}>
-            <span className="mt-4 text-xl md:text-2xl lg:text-3xl inline-block w-full md:w-1/2 lg:w-2/3">
+            <span className="mt-4 lg:mt-6 text-xl md:text-2xl lg:text-3xl inline-block w-full md:w-2/3 lg:w-4/5">
               EXPERIENCE THE FUTURE OF TECHNOLOGY
             </span>
           </div>
